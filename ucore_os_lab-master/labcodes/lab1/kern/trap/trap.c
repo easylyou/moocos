5aa43fc5e9781d052de572cf06d8e93ff4ced8b9
song <sjyfok@163.com>
2016-03-15 22:46:26 +0800

    lab1 ex6 is done

--- ucore_os_lab-master/labcodes/lab1/kern/trap/trap.c
+++ ucore_os_lab-master/labcodes/lab1/kern/trap/trap.c
@@ -31,6 +31,7 @@ static struct pseudodesc idt_pd = {
     sizeof(idt) - 1, (uintptr_t)idt
 };
 
+extern uintptr_t __vectors[];
 /* idt_init - initialize IDT to each of the entry points in kern/trap/vectors.S */
 void
 idt_init(void) {
@@ -46,6 +47,12 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
+    int i = 0;
+	for (i = 0; i < 256; i ++) 
+	{
+		SETGATE(idt[i], 1, 8, __vectors[i], 0); 
+	}
+	asm("lidt idt_pd");
 }
 
 static const char *
@@ -147,6 +154,8 @@ trap_dispatch(struct trapframe *tf) {
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
+        ticks ++;
+       	print_ticks();
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
